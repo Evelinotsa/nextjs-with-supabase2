e9cb36bf341960e47703afe63d6f925679465b33
Evelinotsa <135664496+Evelinotsa@users.noreply.github.com>
2024-10-11 10:21:15 +0300

    feat: add client and server todo components

---
+++ app/todo/page.tsx
@@ -0,0 +1,11 @@
+import ClientComponentTodo from "@/components/todo/client-component";
+import ServerComponentTodo from "@/components/todo/server-component";
+
+export default async function Index() {
+  return (
+    <>
+      <ServerComponentTodo/>
+      <ClientComponentTodo/>
+    </>
+  );
+}
